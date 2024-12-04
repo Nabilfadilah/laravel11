c2f3bbdf5b68998682852fb1c1629bd01b2a9bd2
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-04 16:15:38 +0700

    Mengembalikan lagi code post, author, dan category untuk eager loading default

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,17 @@ Route::get('/about', function () {
 // ontact, email, password
 Route::get('/posts', function () {
     // kalau mau 2 relasi, maka kasih dengan array ['relasi'...]
-    $post = Post::with(['author', 'category'])->latest()->get(); // jika ingin menambahkan sesuatu dalam query, maka tambah get() kalau hanya select * aja maka pake all()
+    // $post = Post::with(['author', 'category'])->latest()->get(); // jika ingin menambahkan sesuatu dalam query, maka tambah get() kalau hanya select * aja maka pake all()
+
     // $post = Post::with('author')->latest()->get(); // jika ingin menambahkan sesuatu dalam query, maka tambah get() kalau hanya select * aja maka pake all()
-    return view('posts', ['title' => 'Blog', 'posts' => $post]);
+
+    $posts = Post::latest()->get();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 // pakai slug untuk membandinkan 
 // Route::get('/posts/{slug}', function ($slug) {
-
 //     $post = Post::find($slug);
-
 //     return view('post', ['title' => 'Single Post', 'post' => $post]);
 // });
 
@@ -37,33 +38,37 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 // untuk melihat post dari satu user apa aja
 Route::get('/authors/{user:username}', function (User $user) {
+    // ----------------------
     // lazy eager loading
-    $posts = $user->posts->load('category', 'author');
+    // $posts = $user->posts->load('category', 'author');
+    // return view('posts', [
+    //     'title' => count($posts) . ' Articles By ' . $user->name,
+    //     'posts' => $posts
+    // ]);
+    // -----------------------
 
     return view('posts', [
-        'title' => count($posts) . ' Articles By ' . $user->name,
-        'posts' => $posts
+        'title' => count($user->posts) . ' Articles By ' . $user->name,
+        'posts' =>
+        $user->posts
     ]);
-    // return view('posts', [
-    //     'title' => count($user->posts) . ' Articles By ' . $user->name,
-    //     'posts' =>
-    //     $user->posts
-    // ]);
 });
 
 // untuk melihat post berdasarkan category
 Route::get('/categories/{category:slug}', function (Category $category) {
+    // --------------------
     // lazy eager loading
-    $posts = $category->posts->load('category', 'author');
+    // $posts = $category->posts->load('category', 'author');
+    // return view('posts', [
+    //     'title' => 'Articles In : ' . $category->name,
+    //     'posts' => $posts
+    // ]);
+    // --------------------
 
     return view('posts', [
         'title' => 'Articles In : ' . $category->name,
-        'posts' => $posts
+        'posts' => $category->posts
     ]);
-    // return view('posts', [
-    //     'title' => 'Articles In : ' . $category->name,
-    //     'posts' => $category->posts
-    // ]);
 });
 
 Route::get('/contact', function () {
