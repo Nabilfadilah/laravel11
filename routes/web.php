9cd2094cb373ada259ef7c82e23cdff8ea13ecb2
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-04 10:09:12 +0700

    Menambah route get untuk melihat post berdasarkan user

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -28,6 +29,15 @@ Route::get('/posts', function () {
 
 Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
+// untuk melihat post dari satu user apa aja
+Route::get('/authors/{user}', function (User $user) {
+    return view('posts', [
+        'title' => 'Articles By ' . $user->name,
+        'posts' =>
+        $user->posts
+    ]);
 });
 
 Route::get('/contact', function () {
