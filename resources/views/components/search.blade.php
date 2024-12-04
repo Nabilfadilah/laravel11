c60a38f908cedc12ae5dcd8d4e7eba7cf6cf7946
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-05 06:06:32 +0700

    Membuat component search

---
+++ resources/views/components/search.blade.php
@@ -0,0 +1,34 @@
+<div class="px-1 mx-auto max-w-screen-xl lg:py-2 lg:px-2">
+    <div class="mx-auto max-w-screen-md sm:text-center">
+        <form action="/posts">
+            @if (request('category'))
+                <input type="hidden" name="category" value="{{ request('category') }}">
+            @endif
+            @if (request('author'))
+                <input type="hidden" name="author" value="{{ request('author') }}">
+            @endif
+            <div class="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
+                <div class="relative w-full">
+                    <label for="search" class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Enter
+                        your artikel</label>
+                    <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                    </div>
+                    <input
+                        class="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        placeholder="Enter your artikel" type="search" id="search" name="search"
+                        autocomplete="off">
+                </div>
+                <div>
+                    <button type="submit"
+                        class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Search</button>
+                </div>
+            </div>
+
+        </form>
+    </div>
+</div>
