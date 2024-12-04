276d25a3db84d71f4e9aedd57964d5e7ec051d26
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-04 10:06:22 +0700

    Menambah function author dengan Belongsto untuk models post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,12 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model // tabel post
 {
     use HasFactory;
 
     protected $fillable = ['title', 'author', 'slug', 'body'];
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     // kalau mau rubah nama tabel, banyak docs di laravel
     // protected $table = 'blog_post';
