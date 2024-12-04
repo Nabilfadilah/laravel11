b23f88a940f8e087e30a5e83b47a2872c5132b94
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-04 08:13:46 +0700

    Menambah hasfactory untuk menangani bug add data post dengan factory

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
-class Post extends Model // tabel post 
+class Post extends Model // tabel post
 {
+    use HasFactory;
+
     protected $fillable = ['title', 'author', 'slug', 'body'];
 
     // kalau mau rubah nama tabel, banyak docs di laravel
