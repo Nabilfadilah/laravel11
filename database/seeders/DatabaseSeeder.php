087fbe314eea68c7d8660fd73aaeed14a02dd174
Nabilfadilah <mohammadfadilah00@gmail.com>
2024-12-04 15:12:37 +0700

    Menggabungkan seeding class database seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +19,54 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // data isi manual
+        // User::create([
+        //     'name' => 'Nabil Fadilah',
+        //     'username' => 'abil',
+        //     'email' => 'nabilfadilah.gmail',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('password'),
+        //     'remember_token' => Str::random(10)
+        // ]);
+
+        // kalau ingin memasukan data user yang di recycke
+        // $nabil = User::create([
+        //     'name' => 'Nabil Fadilah',
+        //     'username' => 'abil',
+        //     'email' => 'nabilfadilah.gmail',
+        //     'email_verified_at' => now(),
+        //     'password' => Hash::make('password'),
+        //     'remember_token' => Str::random(10)
+        // ]);
+
+        // Category::create([
+        //     'name' => 'Web Design',
+        //     'slug' => 'web-design'
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Artikel 1',
+        //     'author_id' => 1,
+        //     'category_id' => 1,
+        //     'slug' => 'judul-artikel-1',
+        //     'body' => 'Non sapiente voluptas itaque nemo nemo. Aliquid quia 
+        //     dolorem omnis ullam. Eos ut quisquam repudianda.'
+        // ]);
+
+        // panggil seeder nya:
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+
+        // siding database
+        Post::factory(50)->recycle([
+            // $nabil, // ikut sertakan variabelnya 
+            // Recycle, dengan cara dipisahkan class nya
+            Category::all(),
+            User::all()
+        ])->create();
     }
 }
